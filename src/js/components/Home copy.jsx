def47a389cbdf61a877b694b5054668c876f73ab
Jaime Vega <jaime.vegah@outlook.com>
2025-12-17 04:26:38 +0000

    modificaciones a la logica y desarrollo de los puntos bonus del ejercicio

--- src/js/components/Home copy.jsx
+++ src/js/components/Home copy.jsx
@@ -4,25 +4,32 @@ import { useState, useEffect  } from 'react';
 
 const Home = () => {
 	let [seconds, setSeconds] = useState(0);
-	let  [index, setIndex] = useState(0);
 	let [cards, setCards] = useState([0,0,0,0,0,0]);
 	useEffect(() => {
 		let timer = setInterval(() => {
-			let segundos = seconds;
-			if (cards[index] < 9) {
-				cards[index] = segundos
-				setCards(cards);
-				setSeconds(seconds + 1);
-			}
-			else {
-				cards[index] = 0
-				setIndex(index+1);
-				cards[index+1] = 1
-				setCards(cards);
-				setSeconds(2);
-			}
-			
-				}, 1000);
+				let segundos = seconds;
+				let arrayCards = cards
+				if (arrayCards[0] < 9 ) {
+					arrayCards[0] = segundos;
+					setCards(arrayCards);
+					setSeconds(seconds + 1);
+				}
+				else {
+					for (let i = 1; i < 6; i++) {
+						if (arrayCards[i] < 9) {
+							arrayCards[i] = arrayCards[i] + 1;
+							break
+						}
+						else {
+							arrayCards[i] = 0
+						}
+					}
+					arrayCards[0] = 0
+					setCards(arrayCards);
+					setSeconds(1);
+				}
+							
+				}, 200);
 		return () => {
 			clearInterval(timer)
 		};
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,42 +3,56 @@ import {Clock} from './Clock';
 import { useState, useEffect  } from 'react';
 
 const Home = () => {
-	let [seconds, setSeconds] = useState(0);
-	let  [index, setIndex] = useState(0);
-	/* let [cards, setCards] = useState([[false,0],[false,0],[false,0],[false,0],[false,0],[false,0]]); */
 	let [cards, setCards] = useState([0,0,0,0,0,0]);
+	let [active, setActive] = useState(true);
 	useEffect(() => {
+		if (active) {
 		let timer = setInterval(() => {
-				let segundos = seconds;
-				if (cards[0] < 9 ) {
-					cards[0] = segundos;
-					setCards(cards);
-					setSeconds(seconds + 1);
+				let arrayCards = [...cards];
+				if (arrayCards[0] < 9 ) {
+					arrayCards[0] = arrayCards[0]+1;
+					setCards(arrayCards);
 				}
 				else {
 					for (let i = 1; i < 6; i++) {
-						if (cards[i] < 9) {
-							cards[i] = cards[i] + 1;
+						if (arrayCards[i] < 9) {
+							arrayCards[i] = arrayCards[i] + 1;
 							break
 						}
 						else {
-							cards[i] = 0
-							setCards(cards);
+							arrayCards[i] = 0
 						}
 					}
-					cards[0] = 0
-					setCards(cards);
-					setSeconds(1);
-				}
-							
-				}, 600);
+					arrayCards[0] = 0
+					setCards(arrayCards);
+				}			
+				}, 500);
 		return () => {
 			clearInterval(timer)
 		};
-	}, [seconds,cards]);
+
+	}
+	}, [cards,active]);
 
 	return (
 		<div className='container'>
+			<div className="buttons2">
+				<button>Cuenta Regresiva</button>
+				<button>Establecer Alerta</button>
+				
+        	</div>
+			<div className="buttons">
+				<button onClick={()=> {
+					setCards([0,0,0,0,0,0]);
+					setActive(true);
+				}}>Reiniciar</button>
+				<button onClick={()=> {
+					setActive(false);
+				}}>Detener</button>
+				<button onClick={()=> {
+					setActive(true);
+				}}>Reanudar</button>
+        	</div>
 			<Clock/>
 			<Number number={cards[5]}/>
 			<Number number={cards[4]}/>
